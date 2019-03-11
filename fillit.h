6ca82ce679fe1672d71bd72f3072631a950872b6
Boris G <21e@list.ru>
2019-03-11 09:27:33 -0700

    Read file. Check block.

--- fillit.h
+++ fillit.h
@@ -1,4 +1,28 @@
 #ifndef FILLIT_H
 # define FILLIT_H
 
+# include "libft/libft.h"
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+
+typedef struct  s_piece
+{
+    int x;
+    int y;
+    int weidh;
+    int heigh;
+    char **map;
+    char letter;
+    struct s_piece *next;
+}               t_piece;
+
+typedef struct  s_game
+{
+    t_piece *pieces;
+    int nb_pieces;
+    int size;
+    char **map;
+}               t_game;
+
 #endif
